168c708de36d0c8e237bc546090c63b571e66444
Umesh <umeshsarkar19@outlook.com>
2023-06-16 00:24:21 +0530

    Implemented basic UI

--- app/src/main/java/com/example/planetext/MainActivity.kt
+++ app/src/main/java/com/example/planetext/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.planetext
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
     }
 }
